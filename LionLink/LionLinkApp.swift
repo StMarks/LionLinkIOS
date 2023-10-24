e10690263295925ad6ddb8434657929c69705f91
James Sabet <jamessabet@gmail.com>
2023-10-23 22:32:50 -0400

    Initial Commit

---
+++ LionLink/LionLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LionLinkApp.swift
+//  LionLink
+//
+//  Created by James Sabet on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct LionLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
